323766a963c7ee82ab951998a7bf01fa6b469fba
jglee96 <zakklee96@gmail.com>
2022-12-19 00:27:58 +0900

    add: 일단 돌아가게
    * react-three fiber typescript 맞추는게 뭔가 어려움

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Mesh } from "three";
+
+function Box(props: JSX.IntrinsicElements["mesh"]) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef<Mesh>(null!);
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+  });
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry
+        // eslint-disable-next-line react/no-unknown-property
+        args={[1, 1, 1]}
+      />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Canvas>
+        <ambientLight />
+        <pointLight
+          // eslint-disable-next-line react/no-unknown-property
+          position={[10, 10, 10]}
+        />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
